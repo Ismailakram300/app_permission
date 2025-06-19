0d9987634240bc2c2573eef9c5bcc6352fcb1ba5
Ismailakram300 <ismailakram300@gmail.com>
2025-06-19 10:25:19 +0500

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:  MyHomeScreen(title: '  Page'),
+      home:  MyHomeScreen(title: 'Home Screen  Page'),
     );
   }
 }
